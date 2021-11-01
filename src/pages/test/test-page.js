2e0b7d378dfbe0a20c5ee28d1fe245a87e1867c8
kala888 <kala.niubility@gmail.com>
2021-11-01 11:46:35 +0800

    add home page ad list

--- src/pages/test/test-page.js
+++ src/pages/test/test-page.js
@@ -1,11 +1,13 @@
 import styles from './styles.less';
 import UserInfoDropdown from '@/components/user-info-dropdown/user-info-dropdown';
+import HomeAdList from '@/components/ad-list/home-ad-list';
 
 
 export default function TestPage() {
 
   return (
     <div className={styles.container}>
+      <HomeAdList />
       <UserInfoDropdown
         name={'周廷志'}
         employeeNo={'11308'}
@@ -13,6 +15,7 @@ export default function TestPage() {
         currentFactory={'珠江工厂'}
         roleList={['司机', '车间主任']}
       />
+
     </div>
   );
 };
